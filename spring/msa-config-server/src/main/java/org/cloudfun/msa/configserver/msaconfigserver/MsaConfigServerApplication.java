b81552f19be76d601cae907425370d62d1b10a62
LotteryOne <xuanfengshizi@163.com>
2017-12-10 14:47:41 +0800

    config-server alpha

--- spring/msa-config-server/src/main/java/org/cloudfun/msa/configserver/msaconfigserver/MsaConfigServerApplication.java
+++ spring/msa-config-server/src/main/java/org/cloudfun/msa/configserver/msaconfigserver/MsaConfigServerApplication.java
@@ -1,12 +1,14 @@
 package org.cloudfun.msa.configserver.msaconfigserver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MsaConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsaConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsaConfigServerApplication.class, args);
+    }
 }
