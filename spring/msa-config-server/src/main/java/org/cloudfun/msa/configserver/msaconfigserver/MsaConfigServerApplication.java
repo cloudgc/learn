b29de2fbbd8893c6599fdc42e653d179c22a90a0
LotteryOne <xuanfengshizi@163.com>
2017-12-10 14:25:36 +0800

    Config-server main class

---
+++ spring/msa-config-server/src/main/java/org/cloudfun/msa/configserver/msaconfigserver/MsaConfigServerApplication.java
@@ -0,0 +1,12 @@
+package org.cloudfun.msa.configserver.msaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaConfigServerApplication.class, args);
+	}
+}
